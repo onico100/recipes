85aae9c1317a38db182097fa500d6006c40fe316
onico100 <142163577+onico100@users.noreply.github.com>
2024-11-11 11:40:40 +0200

    xgfx

---
+++ src/services/mongo.ts
@@ -0,0 +1,31 @@
+export const databaseName = "recipes-app";
+
+const { MongoClient, ObjectId } = require("mongodb");
+
+// export async function connectDatabase() {
+//   const dbConnection: any = process.env.PUBLIC_DB_CONNECTION;
+//   console.log("Connected to database");
+//   return await MongoClient.connect(dbConnection);
+// }
+
+export async function connectDatabase() {
+  const dbConnection: any = process.env.PUBLIC_DB_CONNECTION;
+  return await MongoClient.connect(dbConnection);
+}
+
+export async function insertDocument(
+  client: any,
+  collection: string,
+  document: object
+) {
+  const db = client.db(databaseName);
+  const result = await db.collection(collection).insertOne(document);
+  return result;
+}
+
+export async function getAllDocuments(client: any, collection: string) {
+  const db = client.db(databaseName);
+  const documents = await db.collection(collection).find().toArray();
+  console.log("Doc: ", documents);
+  return documents;
+}
